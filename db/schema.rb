5825bc407276c4ef823b331e3180054977ea312c
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-07-04 18:02:20 -0400

    ran migration for wallets; username to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703173225) do
+ActiveRecord::Schema.define(version: 20140704192911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,18 @@ ActiveRecord::Schema.define(version: 20140703173225) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "wallets", force: true do |t|
+    t.integer  "user_id"
+    t.decimal  "balance_btc"
+    t.decimal  "balance_usd"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
