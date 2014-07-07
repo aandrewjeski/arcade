291fd6a10f7672647d81bae024871072453d7c88
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-07-07 19:32:07 -0400

    added trade model testing

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Trade, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Contact do
+  it "has a valid factory" do
+    Factory(:trade).should be_valid
+  end
+
+  it "is invalid without an amount" do
+    Factory.build(:trade, amount: nil).should_not be_valid
+  end
+
+  it "is invalid without a user id" do
+    Factory.build(:trade, user_id: nil).should_not be_valid
+  end
+
+  it "is invalid without a lastname" do
+    Factory.build(:trade, trade_type: nil).should_not be_valid
+  end
+
 end
