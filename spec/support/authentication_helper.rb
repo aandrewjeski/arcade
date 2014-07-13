478b80e06dedd14b893e755f9ccade1697d7a5a4
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-07-13 18:34:28 -0400

    fixed devise views tests to work with bootstrap

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -2,7 +2,6 @@ module AuthenticationHelper
   def sign_in_as(user)
     visit new_user_session_path
 
-    fill_in 'Username', with: user.username
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
 
